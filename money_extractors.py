a762b104ed141b786971a60a98adc10577847a67
AndreyKolomiets <ak.519@yandex.ru>
2019-06-17 19:45:02 +0300

    Прототип парсера вилок (чуть допиленный MoneyRangeExtractor из наташи)

---
+++ money_extractors.py
@@ -0,0 +1,412 @@
+# coding: utf-8
+from __future__ import unicode_literals, division
+
+import re
+
+from yargy import (
+    rule,
+    and_, or_,
+)
+from yargy.interpretation import (
+    fact,
+    const
+)
+from yargy.predicates import (
+    eq, length_eq,
+    in_, in_caseless,
+    gram, type,
+    normalized, caseless, dictionary
+)
+from yargy.pipelines import morph_pipeline
+
+from natasha.utils import Record
+
+from natasha.extractors import Extractor
+
+from natasha.dsl import (
+    Normalizable,
+    money as dsl
+)
+
+Money = fact(
+    'Money',
+    ['integer', 'fraction', 'multiplier', 'currency', 'coins']
+)
+
+
+class Money(Money, Normalizable):
+    @property
+    def normalized(self):
+        amount = self.integer
+        if self.fraction:
+            amount += self.fraction / 100
+        if self.multiplier:
+            amount *= self.multiplier
+        if self.coins:
+            amount += self.coins / 100
+        return dsl.Money(amount, self.currency)
+
+
+Rate = fact(
+    'Rate',
+    ['money', 'period']
+)
+
+
+class Rate(Rate, Normalizable):
+    @property
+    def normalized(self):
+        return dsl.Rate(
+            self.money.normalized,
+            self.period
+        )
+
+
+Taxation = fact('Taxation', ['taxation'])
+
+Range = fact(
+    'Range',
+    ['min', 'max', 'taxation']
+)
+
+
+class Range(Range, Normalizable):
+    @property
+    def normalized(self):
+        min = self.min.normalized
+        max = self.max.normalized
+        # Приводим к одному масштабу (для вилок типа 150-250 т.р.)
+        if max.amount / min.amount > 10:
+            min.amount *= 1000
+        elif min.amount / max.amount > 10:
+            max.amount *= 1000
+        if not min.currency:
+            min.currency = max.currency
+        return dsl.Range(min, max)
+
+
+DOT = eq('.')
+INT = type('INT')
+
+########
+#
+#   CURRENCY
+#
+##########
+
+
+EURO = or_(
+    normalized('евро'),
+    eq('€')
+).interpretation(
+    const(dsl.EURO)
+)
+
+DOLLARS = or_(
+    normalized('доллар'),
+    eq('$')
+).interpretation(
+    const(dsl.DOLLARS)
+)
+
+RUBLES = or_(
+    rule(normalized('рубль')),
+    rule(
+        or_(
+            caseless('руб'),
+            caseless('р'),
+            eq('₽')
+        ),
+        DOT.optional()
+    )
+).interpretation(
+    const(dsl.RUBLES)
+)
+
+CURRENCY = or_(
+    EURO,
+    DOLLARS,
+    RUBLES
+).interpretation(
+    Money.currency
+)
+
+KOPEIKA = or_(
+    rule(normalized('копейка')),
+    rule(
+        or_(
+            caseless('коп'),
+            caseless('к')
+        ),
+        DOT.optional()
+    )
+)
+
+CENT = or_(
+    normalized('цент'),
+    eq('¢')
+)
+
+EUROCENT = normalized('евроцент')
+
+COINS_CURRENCY = or_(
+    KOPEIKA,
+    rule(CENT),
+    rule(EUROCENT)
+)
+
+############
+#
+#  MULTIPLIER
+#
+##########
+
+
+MILLIARD = or_(
+    rule(caseless('млрд'), DOT.optional()),
+    rule(normalized('миллиард'))
+).interpretation(
+    const(10 ** 9)
+)
+
+MILLION = or_(
+    rule(caseless('млн'), DOT.optional()),
+    rule(normalized('миллион'))
+).interpretation(
+    const(10 ** 6)
+)
+
+THOUSAND = or_(
+    rule(caseless('т'), DOT),
+    rule(caseless('тыс'), DOT.optional()),
+    rule(normalized('тысяча'))
+).interpretation(
+    const(10 ** 3)
+)
+
+MULTIPLIER = or_(
+    MILLIARD,
+    MILLION,
+    THOUSAND
+).interpretation(
+    Money.multiplier
+)
+
+########
+#
+#  NUMERAL
+#
+#######
+
+
+NUMR = or_(
+    gram('NUMR'),
+    # https://github.com/OpenCorpora/opencorpora/issues/818
+    dictionary({
+        'ноль',
+        'один'
+    }),
+)
+
+MODIFIER = in_caseless({
+    'целых',
+    'сотых',
+    'десятых'
+})
+
+PART = or_(
+    rule(
+        or_(
+            INT,
+            NUMR,
+            MODIFIER
+        )
+    ),
+    MILLIARD,
+    MILLION,
+    THOUSAND,
+    CURRENCY,
+    COINS_CURRENCY
+)
+
+BOUND = in_('()//')
+
+NUMERAL = rule(
+    BOUND,
+    PART.repeatable(),
+    BOUND
+)
+
+
+#######
+#
+#   AMOUNT
+#
+########
+
+
+def normalize_integer(value):
+    integer = re.sub('[\s.,]+', '', value)
+    return int(integer)
+
+
+def normalize_fraction(value):
+    fraction = value.ljust(2, '0')
+    return int(fraction)
+
+
+PART = and_(
+    INT,
+    length_eq(3)
+)
+
+SEP = in_(',.')
+
+INTEGER = or_(
+    rule(INT),
+    rule(INT, PART),
+    rule(INT, PART, PART),
+    rule(INT, SEP, PART),
+    rule(INT, SEP, PART, SEP, PART),
+).interpretation(
+    Money.integer.custom(normalize_integer)
+)
+
+FRACTION = and_(
+    INT,
+    or_(
+        length_eq(1),
+        length_eq(2)
+    )
+).interpretation(
+    Money.fraction.custom(normalize_fraction)
+)
+
+AMOUNT = rule(
+    INTEGER,
+    rule(
+        SEP,
+        FRACTION
+    ).optional(),
+    MULTIPLIER.optional(),
+    NUMERAL.optional()
+)
+
+COINS_INTEGER = and_(
+    INT,
+    or_(
+        length_eq(1),
+        length_eq(2)
+    )
+).interpretation(
+    Money.coins.custom(int)
+)
+
+COINS_AMOUNT = rule(
+    COINS_INTEGER,
+    NUMERAL.optional()
+)
+
+#########
+#
+#   MONEY
+#
+###########
+
+
+MONEY = rule(
+    AMOUNT,
+    CURRENCY,
+    COINS_AMOUNT.optional(),
+    COINS_CURRENCY.optional()
+).interpretation(
+    Money
+)
+
+###########
+#
+#   RATE
+#
+##########
+
+
+RATE_MONEY = MONEY.interpretation(
+    Rate.money
+)
+
+PERIODS = {
+    'день': dsl.DAY,
+    'сутки': dsl.DAY,
+    'час': dsl.HOUR,
+    'смена': dsl.SHIFT
+}
+
+PERIOD = dictionary(
+    PERIODS
+).interpretation(
+    Rate.period.normalized().custom(PERIODS.__getitem__)
+)
+
+PER = or_(
+    eq('/'),
+    in_caseless({'в', 'за'})
+)
+
+RATE = rule(
+    RATE_MONEY,
+    PER,
+    PERIOD
+).interpretation(
+    Rate
+)
+
+#######
+#
+#   RANGE
+#
+########
+
+
+DASH = eq('-')
+
+RANGE_MONEY = rule(
+    AMOUNT,
+    CURRENCY.optional()
+).interpretation(
+    Money
+)
+
+RANGE_MIN = rule(
+    eq('от').optional(),
+    RANGE_MONEY.interpretation(
+        Range.min
+    )
+)
+
+RANGE_MAX = rule(
+    eq('до').optional(),
+    RANGE_MONEY.interpretation(
+        Range.max
+    )
+)
+
+TAXATION = rule(dictionary({'чистыми', "грязными",
+                            # "до налогов", "после налогов",
+                            "gross", "гросс", 'net',
+                            # "до НДФЛ", "после НДФЛ",
+                            # "до вычета НДФЛ", "после вычета НДФЛ"
+                            }).interpretation(Range.taxation))
+
+RANGE = rule(
+    RANGE_MIN,
+    DASH.optional(),
+    RANGE_MAX,
+    TAXATION.optional()
+).interpretation(
+    Range
+)
+
+
+class MoneyRangeExtractor(Extractor):
+    def __init__(self):
+        super(MoneyRangeExtractor, self).__init__(RANGE)
---
+++ test_money_extractors.py
@@ -0,0 +1,65 @@
+from money_extractors import MoneyRangeExtractor
+
+
+ext = MoneyRangeExtractor()
+# ['от 60К до 300К', '120т.р. - 160 т.р.',
+#  'от 100 до 150', '$5k–$8k', 'от 2к до 4к Евро',
+#  '4000-5500 $', 'Вилка :fork:: 100-250 тысяч рублей на руки', '2-4k USD', '150-250 т.р. «чистыми»',
+# ''':fork::fork::fork:
+# *80-90+k€*''']
+
+
+def test_pattern1():
+    text = 'от 60К до 300К'
+    matches = ext(text).as_json
+    print(matches)
+
+
+def test_pattern5():
+    text = 'от 60к до 300к'
+    matches = ext(text).as_json
+    print(matches)
+
+
+def test_pattern2():
+    text = '120т.р. - 160 т.р. чистыми'
+    matches = ext(text).as_json
+    assert len(matches) != 0
+    match = matches[0]['fact']
+    assert match['min']['amount'] == 120000
+    assert match['min']['currency'] == 'RUB'
+    assert match['max']['amount'] == 160000
+    assert match['max']['currency'] == 'RUB'
+    print(matches)
+
+
+def test_pattern3():
+    text = '$5k–$8k'
+    matches = ext(text).as_json
+    assert len(matches) != 0
+    match = matches[0]['fact']
+    assert match['min']['amount'] == 5000
+    assert match['min']['currency'] == 'USD'
+    assert match['max']['amount'] == 8000
+    assert match['max']['currency'] == 'USD'
+    print(matches)
+
+
+def test_pattern4():
+    text = '150-250 т.р. «чистыми»'
+    matches = ext(text).as_json
+    print(matches)
+    assert len(matches) != 0
+    match = matches[0]['fact']
+    assert match['min']['amount'] == 150000
+    assert match['min']['currency'] == 'RUB'
+    assert match['max']['amount'] == 250000
+    assert match['max']['currency'] == 'RUB'
+
+
+if __name__ == '__main__':
+    test_pattern5()
+    test_pattern1()
+    test_pattern2()
+    test_pattern4()
+
